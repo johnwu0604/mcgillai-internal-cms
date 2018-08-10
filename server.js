59f144bb1fe29d78d68987d4cc0cac9da578cf83
John Wu <john.wu@mail.mcgill.ca>
2018-08-09 23:44:39 -0700

    Added react

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser')
 var app = express()
 var port = process.env.PORT || 5000
 
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({'extended': 'true'}))
 app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+var config = {
+  entry: './public/main.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'public/index.js'
+  },
+  mode: "development",
+  devServer: {
+    inline: true,
+    port: 5000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config
